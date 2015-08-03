19335780c368effbb3eaf4e5ebb0d453d9dac6a4
Curtis Maloney <curtis@tinbrain.net>
2015-08-03 14:24:10 +1000

    Use valid imports

--- dumpling/views.py
+++ dumpling/views.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 
-from scss import Compiler, Namespace, String
+from scss import Compiler
+from scss.namespace import Namespace
+from scss.types import String
 
 from .models import Page, ThemeValue
 
