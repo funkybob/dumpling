2e16dcf48186187788aa567303ff269a6618cf71
Curtis Maloney <curtis@tinbrain.net>
2015-08-03 14:06:30 +1000

    Made admin usable

--- dumpling/admin.py
+++ dumpling/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
+from django_hstore.widgets import AdminHStoreWidget
+
 from . import models
 
 
 class PageWidgetInline(admin.TabularInline):
     model = models.PageWidget
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('url', 'title', 'is_published',)
@@ -12,6 +15,21 @@ class PageAdmin(admin.ModelAdmin):
     inlines = [
         PageWidgetInline,
     ]
+    formfield_overrides = {
+        models.HStoreField: {'widget': AdminHStoreWidget},
+    }
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('parent', 'path'),
+                'title',
+                'template',
+                ('is_published', 'order'),
+                'fragments',
+            ),
+        }),
+    )
+
 
 admin.site.register(models.Page, PageAdmin)
 admin.site.register(models.PageWidget)
