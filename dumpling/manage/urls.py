7fbb6cb91e6d799fe8c8e121ab9c31069f533317
Curtis Maloney <curtis@tinbrain.net>
2015-09-05 10:12:49 +1000

    Start adding template editing UI

--- dumpling/manage/urls.py
+++ dumpling/manage/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^templates/$', views.TemplateList.as_view()),
 ]
--- dumpling/manage/views.py
+++ dumpling/manage/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+import os
 
+from django.conf import settings
+from django.shortcuts import render
+from django.utils._os import safe_join
+
+from nap.rest import views
+
+from . import mappers
 from ..models import Page
 
 
@@ -7,3 +14,17 @@ def index(request):
     return render(request, 'dumpling/index.html', {
         'roots': Page.objects.filter(parent=None),
     })
+
+
+class TemplateList(views.BaseListView):
+    mapper_class = mappers.PageMapper
+
+    def get(self, request, *args, **kwargs):
+        root = kwargs.get('root', '')
+        base_path = safe_join(settings.USER_TEMPLATES_PATH, root)
+
+        return self.multiple_response(object_list=[
+            (fn, os.stat(os.path.join(base_path, fn)))
+            for fn in os.listdir(base_path)
+            if not fn.startswith('.')
+        ])
