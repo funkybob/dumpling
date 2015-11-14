976c3915e8ae96b916faca02957a7c98af466256
Curtis Maloney <curtis@tinbrain.net>
2015-11-14 20:25:10 +1100

    Add file info generator. Convert views to use it. Switch to streaming views

--- dumpling/manage/urls.py
+++ dumpling/manage/urls.py
@@ -1,9 +1,10 @@
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^templates/$', views.TemplateList.as_view()),
+    url(r'^media/$', views.MediaList.as_view()),
 ]
--- dumpling/manage/views.py
+++ dumpling/manage/views.py
@@ -1,13 +1,15 @@
 import os
+import mimetypes
 
 from django.conf import settings
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 from django.utils._os import safe_join
 
 from nap.rest import views
 
-from . import mappers
 from ..models import Page
+from ..utils import json
 
 
 def index(request):
@@ -17,14 +19,45 @@ def index(request):
 
 
 class TemplateList(views.BaseListView):
-    mapper_class = mappers.PageMapper
 
     def get(self, request, *args, **kwargs):
         root = kwargs.get('root', '')
         base_path = safe_join(settings.USER_TEMPLATES_PATH, root)
 
-        return self.multiple_response(object_list=[
-            (fn, os.stat(os.path.join(base_path, fn)))
-            for fn in os.listdir(base_path)
-            if not fn.startswith('.')
-        ])
+        return StreamingHttpResponse(
+            json.dumps(dir_meta(base_path)),
+            content_type='application/json',
+        )
+
+
+class MediaList(views.BaseListView):
+
+    def get(self, request, *args, **kwargs):
+
+        return StreamingHttpResponse(
+            json.dumps(dir_meta(settings.MEDIA_ROOT)),
+            content_type='application/json',
+        )
+
+
+def dir_meta(path):
+    '''
+        {
+            name: filename,
+            path: path/from/root/filename
+            content_type: 'image/jpeg',
+            size: in_bytes,
+        }
+    '''
+    pfx_len = len(path)
+    for dirpath, dirnames, filenames in os.walk(path):
+        for filename in filenames:
+            full_name = os.path.join(dirpath, filename)
+            stat = os.stat(full_name)
+            yield {
+                'name': filename,
+                'path': full_name[pfx_len:],
+                'content_type': mimetypes.guess_type(full_name)[0],
+                'size': stat.st_size,
+                'modified': stat.st_mtime,
+            }
